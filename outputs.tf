46fa3167fea11e18ac08535b40e46fc4daeb609d
mangofunky <mangofunky@hotmail.com>
2023-12-20 20:57:25 +0000

    28 s3 static website hosting (#29)
    * #28 created S3 bucket for static hosting part 1
    
    * #28 enabled static website hosting, uploaded index and error files via terraform, documented

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "bucket_name" {
     value = module.terrahouse_aws.bucket_name
 }
+
+output "website_endpoint" {
+    description = "S3 Static Website hosting endpoint"
+    value = module.terrahouse_aws.website_endpoint  
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,9 @@ variable "user_uuid" {
 variable "bucket_name" {
     type = string
 }
+variable "index_html_filepath" {
+  type = string
+}
+variable "error_html_filepath" {
+  type = string
+}
