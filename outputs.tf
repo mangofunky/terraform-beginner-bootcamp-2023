4ca42d20df192ebc20342f4c1281bfbe6b9d3038
mangofunky <mangofunky@hotmail.com>
2023-12-22 15:28:21 +0000

    #32 Invalidate Cloudfront stack when content version changes

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "website_endpoint" {
     description = "S3 Static Website hosting endpoint"
     value = module.terrahouse_aws.website_endpoint  
 }
+output "cloudfront_url" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.terrahouse_aws.cloudfront_url
+}
