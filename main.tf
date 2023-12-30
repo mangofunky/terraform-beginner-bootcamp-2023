bbc79b91c95d05e542535f6b2af082aad993b7ba
mangofunky <mangofunky@hotmail.com>
2023-12-30 12:50:31 +0000

    42 terratowns provider (#44)
    * #42 WIP setting up skeleton for creating a custom terraform provider
    
    * #42 enabled TF_LOG=BEBUG and commented out outputs. Custom Provider block working correctly

--- main.tf
+++ main.tf
@@ -16,7 +16,8 @@ terraform {
 
 provider "terratowns" {
   endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  # user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
+  user_uuid="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
 
