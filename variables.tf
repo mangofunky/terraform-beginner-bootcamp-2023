0f38c8bbf0bb390711250793b8097bf72282dd07
mangofunky <mangofunky@hotmail.com>
2023-12-31 21:40:28 +0000

    47 terratowns test (#48)
    * #47 Terratown test part1
    
    * #47 test our terra home on terratowns test town production server

--- variables.tf
+++ variables.tf
@@ -1,9 +1,15 @@
-variable "user_uuid" {
-    type = string
+variable "terratowns_endpoint" {
+ type = string
 }
-variable "bucket_name" {
-    type = string
+variable "terratowns_access_token" {
+ type = string
 }
+variable "teacherseat_user_uuid" {
+ type = string
+}
+# variable "bucket_name" {
+#     type = string
+# }
 variable "index_html_filepath" {
   type = string
 }
